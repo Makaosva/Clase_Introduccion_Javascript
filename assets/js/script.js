dabacc9bc9f95971d3c0b5820c57d4b51be7ba65
Makaosva <maka.osses@gmail.com>
2024-03-25 22:42:54 -0300

    feat/VAriables en JavaScript

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+/*  Valores Primitivo*/
+
+/*Numeros*/
+3 + (2 * 3) / 1;
+//devuelve --->16;
+3 + 2 * 3 + 1;
+//devuelve -->10
+
+/* string */
+//"Hola";
+// "100";
+// "true";
+// "Hola. soy un string";
+
+/* Booleanos */
+true;
+false;
+
+typeof 12;
+//devuelve --> 'number'
+typeof "hola";
+//devuelve un steing
+typeof true;
+//devuelve un boolean
+typeof "12"
+//devuelve un string
+
+/* Mensaje de Consola*/
+console.log("Soy un mensaje de Consola");
+
+/* ALERTAS  */
+alert("Soy un mensaje de alerta");
+confirm("Acepta los terminos y condiciones");
+prompt("Dime como te llamas");
+
+/* VARIABLES */
+let numero = 23
+
+console.log(numero);
+console.log(numero * 10);

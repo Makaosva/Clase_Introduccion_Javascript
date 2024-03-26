9d2fbc8210ed3092ef9f91c0f7d94b4a7312c001
Makaosva <maka.osses@gmail.com>
2024-03-25 23:27:40 -0300

    feat/Concatenacion

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,19 +22,49 @@ typeof "hola";
 //devuelve un steing
 typeof true;
 //devuelve un boolean
-typeof "12"
+typeof "12";
 //devuelve un string
 
 /* Mensaje de Consola*/
 console.log("Soy un mensaje de Consola");
 
 /* ALERTAS  */
-alert("Soy un mensaje de alerta");
+/*alert("Soy un mensaje de alerta");
 confirm("Acepta los terminos y condiciones");
-prompt("Dime como te llamas");
+prompt("Dime como te llamas");*/
 
 /* VARIABLES */
-let numero = 23
+let numero = 23;
+console.log(numero, "<-- solo numero");
+console.log(numero * 10, "<-- numero multiplicado por 10");
 
-console.log(numero);
-console.log(numero * 10);
+//redeclarar el valor
+numero = 245;
+console.log("numero modificado -->", numero);
+
+//var string = "hola"; con var se puede volver a declarar, y no genera error
+let persona = "Macarena";
+console.log (persona, "<-- persona");
+console.log("nombre");
+
+//alert(persona);
+
+/*----------------------*/
+
+let nombre = prompt("Como te llamas?");
+let edad = prompt("Dime tu edad");
+console.log (nombre,"<--nombre");
+
+/* CONCANETACION*/
+console.log("Hola " + nombre);
+console.log("Tengo " + edad + " años");
+console.log("En 10 años vas a tener " + edad + 10 + " años");
+
+console.log(5 + 5);
+console.log("5" + "5");
+console.log(5 + "5");
+console.log("5 + 5");
+
+let sumaDeAnios = Number(edad) + 10;
+console.log("En 10 años vas a tener " + sumaDeAnios + " años");
+//console.log(sumaDeAnios);
